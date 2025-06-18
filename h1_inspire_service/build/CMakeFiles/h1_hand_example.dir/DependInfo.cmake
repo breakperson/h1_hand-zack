54ca77795d68d1d824b24af48cec09256694c73f
breakperson <104192923+breakperson@users.noreply.github.com>
2025-06-18 14:13:37 +0800

    Add files via upload

---
+++ h1_inspire_service/build/CMakeFiles/h1_hand_example.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zack/h1_inspire_service/example/h1_hand_example.cpp" "/home/zack/h1_inspire_service/build/CMakeFiles/h1_hand_example.dir/example/h1_hand_example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/ddscxx"
+  "/usr/local/include/iceoryx/v2.0.2"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ h1_inspire_service/build/CMakeFiles/h1_hand_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h1_hand_example.dir/example/h1_hand_example.cpp.o"
+  "h1_hand_example"
+  "h1_hand_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/h1_hand_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
